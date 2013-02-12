0db1a65a9c88006e6abd1c1f59a4b0955c947e49
David Adair <adair.david@gmail.com>
2013-02-12 16:13:09 -0700

    Added sample unit test to item.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
   attr_accessible :title, :genre, :isbn13, :isbn10, :publisher, :year
+
+  validates :title,  :presence => true
+  validates :isbn13, :presence => true
+  validates :isbn10, :presence => true
 end
