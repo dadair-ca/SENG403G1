bc987bcd35d6a0a2dfe3f75fd60f5ff95f263258
David Adair <adair.david@gmail.com>
2013-02-13 00:09:02 -0700

    Made home#index the default home again.

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,8 @@ SENG403G1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'items#index'
+  #root :to => 'items#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
