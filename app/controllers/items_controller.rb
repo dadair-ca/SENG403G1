aa8cab587b716739af8d9a507e488ee540f450a3
David Adair <adair.david@gmail.com>
2013-02-12 21:32:03 -0700

    Added validation of unique barcodes. Added CSS rule for even/odd TD items.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,7 @@ class ItemsController < ApplicationController
   # GET /items/1.json
   def show
     @item = Item.find(params[:id])
+    @copies = @item.physical_items.all
 
     respond_to do |format|
       format.html # show.html.erb
--- app/models/physical_item.rb
+++ app/models/physical_item.rb
@@ -1,4 +1,5 @@
 class PhysicalItem < ActiveRecord::Base
   belongs_to :item
   attr_accessible :barcode_id
+  validates :barcode_id, :uniqueness => true
 end
