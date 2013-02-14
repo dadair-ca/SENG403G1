c167e70fa2a45e54d4436d64b155f8ad5fff62f3
David Adair <adair.david@gmail.com>
2013-02-14 16:30:37 -0700

    Added seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+hobbit = Item.create(:title => "The Hobbit", :genre => "Fantasy", :isbn13 => 13, :isbn10 => 10)
+hobbit.authors.create(:given_name => "J.R.R.", :surname => "Tolkien")
