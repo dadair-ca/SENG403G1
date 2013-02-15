920884c50ad1fe282d7d9b2368057fcccd0f48cb
Gellert Kispal <ace_da_tker@yahoo.com>
2013-02-15 13:16:29 -0700

    temp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130214042524) do
+ActiveRecord::Schema.define(:version => 20130214231554) do
 
   create_table "authoreds", :force => true do |t|
     t.integer  "item_id"
--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
    test "should not save item without title" do
-     item = Item.new
+     item = Item.new(:title => "")
      assert !item.save
    end
 end
