4a7e7344d73fe3e6d375bc74d27ed76e15a09c3f
Gell <paul_g052006@yahoo.com>
2013-02-16 14:10:57 -0700

    Added an test case for duplicate authors

--- test/unit/author_test.rb
+++ test/unit/author_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "should not insert blank author" do
+        author = Author.new
+        assert !author.save
+    end
+
+   test "should not save duplicate authors" do
+     author1 = Author.new(:given_name => "Mike", :surname => "Author")
+     author1.save
+     author2 = Author.new(:given_name => "Mike", :surname => "Author")
+     assert !author2.save
+   end
+
 end
