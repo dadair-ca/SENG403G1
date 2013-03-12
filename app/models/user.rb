c9e3a68befdcd58a9c36aff8bc6c0087a391504e
sapratte <sydney.pratte@hotmail.com>
2013-03-12 16:32:14 -0600

    no change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+
+  has_many :rentals
+  has_many :physical_items, :through => :rentals
+  
+  validates_presence_of :given_name
+  validates_presence_of :surname
+  validates_presence_of :category
+  validates_presence_of :email
+
+  validates_uniqueness_of :email
+
+  def category_as_string
+    return "Patron" if self.category == 0
+    return "Librarian" if self.category == 1
+    return "Admin" if self.category == 2
+  end
+  
+  
 end
