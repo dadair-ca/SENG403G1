8d09480de9ca9acd2ed15517872effa2fe5d59c9
Shena <shena.fortozo@gmail.com>
2013-03-12 19:14:53 -0600

    Added seed data for physical and rental items

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,18 @@ tolkien = Author.create(:given_name => "J.R.R.", :surname => "Tolkien")
 tolkien.items = lotrs
 
 #Remember that minimum password requirement is 6 characters
-User.create(:category => 0, :given_name => "David", :surname => "Adair", :email => "notreal@fake.com", :password => "aaabbbccc", :password_confirmation => "aaabbbccc")
-User.create(:category => 1, :given_name => "Shena", :surname => "Fortozo", :email => "fake@fake.com")
-User.create(:category => 2, :given_name => "Gellert", :surname => "Kispal", :email => "false@fake.com")
-User.create(:category => 2, :given_name => "Sydney", :surname => "Pratte", :email => "nottrue@fake.com")
-User.create(:category => 1, :given_name => "Ho Wai", :surname => "Yung", :email => "notnotfalse@fake.com")
+david = User.create(:category => 0, :given_name => "David", :surname => "Adair", :email => "notreal@fake.com", :password => "aaabbbccc", :password_confirmation => "aaabbbccc")
+shena = User.create(:category => 1, :given_name => "Shena", :surname => "Fortozo", :email => "fake@fake.com", :password => "aaabbbccc", :password_confirmation => "aaabbbccc")
+User.create(:category => 2, :given_name => "Gellert", :surname => "Kispal", :email => "false@fake.com", :password => "dddeeefff", :password_confirmation => "aaabbbccc")
+User.create(:category => 2, :given_name => "Sydney", :surname => "Pratte", :email => "nottrue@fake.com", :password => "aaabbbccc", :password_confirmation => "aaabbbccc")
+User.create(:category => 1, :given_name => "Ho Wai", :surname => "Yung", :email => "notnotfalse@fake.com", :password => "aaabbbccc", :password_confirmation => "aaabbbccc")
+
+Item.first.physical_items.create(:barcode_id => 111)
+Item.first.physical_items.create(:barcode_id => 222)
+Item.first.physical_items.create(:barcode_id => 333)
+Item.find(2).physical_items.create(:barcode_id => 444)
+
+Rental.create(:user_id => david.id, :barcode_id => 111, :renewals => 1, :return_date => 10.days.from_now, :rent_date => Time.now)
+Rental.create(:user_id => shena.id, :barcode_id => 222, :renewals => 2, :return_date => 20.days.from_now, :rent_date => Time.now)
+Rental.create(:user_id => shena.id, :barcode_id => 333, :renewals => 2, :return_date => 15.days.from_now, :rent_date => Time.now)
+Rental.create(:user_id => shena.id, :barcode_id => 444, :renewals => 3, :return_date => 30.days.ago, :rent_date => 60.days.ago)
