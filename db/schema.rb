b3dd53fac8fe9b3f2a60404b257b3b81844dd39f
sapratte <sydney.pratte@hotmail.com>
2013-03-14 08:19:59 -0600

    Changed form layout for new rental

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -33,8 +32,10 @@ ActiveRecord::Schema.define(:version => 20130312211518) do
   end
 
   create_table "mailers", :force => true do |t|
-    t.string "subject"
-    t.text   "body",    :limit => 255
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "physical_items", :force => true do |t|
