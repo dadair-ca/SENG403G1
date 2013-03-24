a00fd2d10a602bf04e489e8d50b505fb3c60da44
David Adair <adair.david@gmail.com>
2013-03-23 21:45:45 -0600

    Fixed syntax errors.

--- test/functional/mailers_controller_test.rb
+++ test/functional/mailers_controller_test.rb
@@ -18,30 +18,30 @@ class MailersControllerTest < ActionController::TestCase
 
   test "should create mailer" do
     assert_difference('Mailer.count') do
-      post :create, mailer: { body: @mailer.body, id: @mailer.id, subject: @mailer.subject }
+      post :create, :mailer => { :body => @mailer.body, :id => @mailer.id, :subject => @mailer.subject }
     end
 
     assert_redirected_to mailer_path(assigns(:mailer))
   end
 
   test "should show mailer" do
-    get :show, id: @mailer
+    get :show, :id => @mailer
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @mailer
+    get :edit, :id => @mailer
     assert_response :success
   end
 
   test "should update mailer" do
-    put :update, id: @mailer, mailer: { body: @mailer.body, id: @mailer.id, subject: @mailer.subject }
+    put :update, :id => @mailer, :mailer => { :body => @mailer.body, :id => @mailer.id, :subject => @mailer.subject }
     assert_redirected_to mailer_path(assigns(:mailer))
   end
 
   test "should destroy mailer" do
     assert_difference('Mailer.count', -1) do
-      delete :destroy, id: @mailer
+      delete :destroy, :id => @mailer
     end
 
     assert_redirected_to mailers_path
