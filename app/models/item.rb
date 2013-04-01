80bd0c023038834822cf42283b584e94a4c496bf
Shena <shena.fortozo@gmail.com>
2013-04-01 11:46:18 -0600

    Fix item.rb merge conflict

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,14 +17,13 @@ class Item < ActiveRecord::Base
   validates_uniqueness_of :title, :scope => [:isbn13, :isbn10, :year]
   validates_uniqueness_of :isbn13
   validates_uniqueness_of :isbn10
-   
   
-  validates :isbn13, :numericality => { :only_integer => true }
-  validates :isbn10, :numericality => { :only_integer => true }
   validates :year, :numericality => { :only_integer => true }
   
   accepts_nested_attributes_for :author
-
+  
+  $stopwords = ["I", "a", "about", "an", "are", "as", "at", "be", "by", "com", "for", "from", "how", "in", "is", "it", "of", "on", "or", "that", "the", "this", "to", "was", "what",  "when", "where", "who",  "will",  "with", "the", "www"]
+  
   # Try to find an existing author rather than creating a duplicate record
   # If one if found, grab the reference.
   def replace_existing_author!
@@ -34,22 +33,146 @@ class Item < ActiveRecord::Base
       self.author = author if author 
     end
   end
+  
+  def self.levenshtein_search(search_terms, search_type)
+    
+    threshold = 2
+    totaldl = 0
+    lowestdl = -1
+    
+    tok_db = search_type.to_s.strip.downcase.split(" ").uniq
+    tok_db = tok_db - $stopwords
+    tok_st = search_terms
+    
+    tok_st.each do |search_tok|
+      tok_db.each do |db_tok|
+        temp = DamerauLevenshtein.distance(db_tok, search_tok, 1, threshold)
+        if(temp == 0)
+          lowestdl = temp
+        elsif((temp < lowestdl) || (lowestdl == -1))
+          lowestdl = temp
+        end
+      end
+      totaldl += lowestdl
+    end
+    
+    return totaldl
+      
+  end
+  
+  def self.levenshtein_isbn(search_terms, search_isbn)
+  
+    tok_db = search_isbn.to_s.strip.downcase
+    tok_st = search_terms[0]
+    
+    threshold = 2
+    totaldl = 0
+    lowestdl = -1
+    
+    db_len = tok_db.length
+    st_len = tok_st.length
+    
+    if db_len < st_len
+      return 3
+    else
+      for i in 0..(db_len-st_len-1)
+        temp_db = tok_db[i..(st_len+i)]
+        temp_dl = DamerauLevenshtein.distance(temp_db, tok_st, 1, threshold)
+        if(temp_dl == 0)
+          lowestdl = temp_dl
+        elsif((temp_dl < lowestdl) || (lowestdl == -1))
+          lowestdl = temp_dl
+        end
+      end
+    end
+    
+    return lowestdl
+  end
 
   #Search all the items based upon the drop down menu selection
   #Note: I am using a bogus year value for the error message, which will
   #display nothing. Later on I'll remodify the view file for search
   #to handle errors.
-  def self.search(search, search_type)
-    eSearch = (Time.now.year + 1).to_s      #Bogue search variable
-    search_str = search.to_s.gsub(/\s+/, ' ')    #Splits and concatenates search string, useful in getting rid of leading spaces.
+  def self.search(search, search_type, id_list)
+    eSearch = (Time.now.year + 1).to_s    #Bogue search variable
+    words = search.to_s.strip.downcase.split(" ").uniq
+    words = words - $stopwords
     
-    if search_str.present?
-      if search_type != 'author'
-        Item.joins(:author).find(:all, :conditions => [search_type + ' LIKE ?', "%#{search_str}%"])
-      else
-        Item.joins(:author).where('given_name LIKE ? or surname LIKE ?', "%#{search_str}%", "%#{search_str}%")
+    threshold = 3
+    @books = self.joins(:author)
+    result = Array.new
+    f_result = Array.new
+    
+    if words.present?
+      if search_type == 'Title'
+        @books.each do |book|
+          if((lev_value = levenshtein_search(words, book.title)) < threshold)
+            result << [book, lev_value]
+          end
+        end
+        
+      elsif search_type == 'Author'
+        @books.each do |book|
+          lev_value = levenshtein_search(words, book.author.given_name)
+          lev_value += levenshtein_search(words, book.author.surname) 
+          if(lev_value < 6)
+            result << [book, lev_value]
+          end
+        end
+        
+      elsif search_type == 'Genre'
+        @books.each do |book|
+          if((lev_value = levenshtein_search(words, book.genre)) < threshold)
+            result << [book, lev_value]
+          end
+        end
+        
+      elsif search_type == 'Publisher'
+        @books.each do |book|
+          if((lev_value = levenshtein_search(words, book.publisher)) < threshold)
+            result << [book, lev_value]
+          end
+        end
+      elsif search_type == 'Year' 
+        if(words.length == 1)
+          if((words[0].to_i) && (words[0].to_i <= Time.now.year))
+            s_year = words[0].to_i
+            @books.each do |book|
+              if(book.year == s_year)
+                result << book
+              end
+            end
+            result.sort_by(&:title)
+            return result
+          end
+        end
+      elsif search_type == 'ISBN 13'
+        @books.each do |book|
+          if((lev_value = levenshtein_isbn(words, book.isbn13)) < threshold)
+            result << [book, lev_value]
+          end
+        end
+      elsif search_type == 'ISBN 10'
+        @books.each do |book|
+          if((lev_value = levenshtein_isbn(words, book.isbn10)) < threshold)
+            result << [book, lev_value]
+          end
+        end
       end
+   
+      result.sort_by(&:second)
+      result.each{|r| f_result.push(r[0])}
+      return f_result
+      
+    else
+      return result
     end
+    
+    
+  end
+  
+  def self.view(search, search_type, id_list)
+      return(search.to_s.strip.split(" ").uniq)
   end
 
 #There are 3 things I can do to search for the authors name
@@ -58,9 +181,16 @@ class Item < ActiveRecord::Base
 #Figure out a way to get the author column to search in for this method
 
   def self.advance_search(title)
-    find(:all, :conditions => ['title LIKE ?', "%#{title}%"]) # temporary/filler
+    @books.each do |book|
+      if((lev_value = levenshtein_search(words, book.title)) < 3)
+        result << [book, lev_value]
+      end
+    end
+    
+    result.sort_by(&:last)
+    result.map {|r| r[0]}
+
+    return result
   end
-  
-  
-  
+
 end
