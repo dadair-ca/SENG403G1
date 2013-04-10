d64cd7c3d244aafe1141b6bc264fef98de1c5065
Shena <shena.fortozo@gmail.com>
2013-04-09 19:17:40 -0600

    Updated overdue notification view

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -3,40 +3,39 @@ class MailersController < ApplicationController
   # GET /rental/1/mailers/overdue.json
   def new
     @id = params[:rental_id]
-    if @id.nil?
-      format.html { redirect_to rentals_path }
-    else
-      @rental = Rental.find(params[:rental_id])
-      @user   = @rental.user
-      @item   = @rental.item
-      @mailer = Mailer.new()
+    @rental = Rental.find(params[:rental_id])
+    @user   = @rental.user
+    @item   = @rental.item
+    
+    @mailer = Mailer.new()
+    @mailer.subject = "[WebLib] Overdue Item"
+    @mailer.body = 
+"Hi #{@user.given_name} #{@user.surname},
 
-      respond_to do |format|
-        format.html # new.html.erb
-        format.json { render :json => @mailer }
-      end
+The item '#{@item.title}' by #{@item.author.given_name} #{@item.author.surname} is overdue on #{@rental.return_date.strftime('%A, %B %d at %I:%M %p')}.
+
+Thanks for using WebLib!"
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render :json => @mailer }
     end
   end
 
-  # POST /rental/1/mailers/
-  # POST /rental/1/mailers/.json
+  # POST /rental/1/mailers
+  # POST /rental/1/mailers.json
   def create
     @method = params[:method]
     @rental = Rental.find(params[:rental])
     @mailer = Mailer.new(params[:mailer])
 
     respond_to do |format|
-      if @method == "custom"
-        if @mailer.valid?
-          UserMailer.custom_email(@mailer, @rental).deliver
-          format.html { redirect_to rentals_url, :notice => 'Mail has been sent.' }
-        else
-          format.html { render :action => "new" }
-          format.json { render :json => @mailer.errors, :status => :unprocessable_entity }
-        end
-      else
-        UserMailer.overdue_email(@rental).deliver
+      if @mailer.valid?
+        UserMailer.custom_email(@mailer, @rental).deliver
         format.html { redirect_to rentals_url, :notice => 'Mail has been sent.' }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @mailer.errors, :status => :unprocessable_entity }
       end
     end
   end
