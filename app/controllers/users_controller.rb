2dff3ec16fd29ffc60c3f65388cae5ef92f50be1
Shena <shena.fortozo@gmail.com>
2013-04-10 23:07:36 -0600

    Standardizing "back" button
    Fixed number of results output in users#index page
    Fixed overdue mailer link in users#show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,10 @@ class UsersController < ApplicationController
       @categories[user.category_as_string] += 1
     end
     @categories = @categories.sort_by { |k,v| k }.sort_by { |k,v| -v }
-
-    @users = @users.paginate(:page => params[:page], :per_page => 10)
     
     @results = @users.size
+
+    @users = @users.paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
